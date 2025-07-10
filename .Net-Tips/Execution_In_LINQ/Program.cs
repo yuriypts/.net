1bc6325e45bd82ea140006f27a3cd18a9ea5e8b4
Yuriy Parashynets <yura.parashynets@gmail.com>
2025-07-10 14:15:51 +0300

    main - update

--- .Net-Tips/Execution_In_LINQ/Program.cs
+++ .Net-Tips/Execution_In_LINQ/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // This happens because of deferred execution in LINQ.The Select projection(var times2) doesn’t evaluate
+            // This happens because of deferred execution in LINQ.The Select projection(IEnumerable<int> numbers2) doesn’t evaluate
             // immediately.It’s re-evaluated each time it’s iterated. So when you modify the original array after
             // defining the LINQ query but before the foreach, the changes are reflected in the output. That’s why 
             // the first number is now doubled as 20, not 2.
