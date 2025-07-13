18ff3868a1809aa0f9a4be005913049db872950b
Yuriy Parashynets <yura.parashynets@gmail.com>
2025-07-13 16:07:58 +0300

    main - update

--- .Net-Tips/Execution_In_LINQ/Program.cs
+++ .Net-Tips/Execution_In_LINQ/Program.cs
@@ -19,6 +19,35 @@
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine(new string('-', 50));
+
+            IEnumerator<int> numbers3 = numbers2.GetEnumerator();
+            while (numbers3.MoveNext())
+            {
+                Console.WriteLine(numbers3.Current);
+            }
+
+            // The Count property is not available for IEnumerable<T>. Instead, use the Count() extension method from LINQ.
+            for (int i = 0; i < numbers2.Count(); i++)
+            {
+                // This will throw an exception because numbers2 is an IEnumerable<int> and does not support indexing.
+                //Console.WriteLine(numbers2[i]);
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+            // This happens because the LINQ query is evaluated immediately when you call ToList().
+            List<int> arrayInts = numbers2.ToList();
+            // After this, if you modify the original array, it won't affect the already evaluated list.
+            IEnumerable<int> filteredInts = arrayInts.Where(x => x > 4);
+
+            //Console.WriteLine(filteredInts[0]);
+
+            foreach (int item in filteredInts)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
