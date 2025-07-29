10f2191b7f0dad10c819bebddf6d8bfa26584a8a
Yuriy Parashynets <yura.parashynets@gmail.com>
2025-07-29 11:36:22 +0300

    main - update

--- .Net-Tips/Closure/Program.cs
+++ .Net-Tips/Closure/Program.cs
@@ -16,6 +16,14 @@
             Func<int> closureFunc2 = ClosureFunc();
             Console.WriteLine(closureFunc2.Invoke());
             Console.WriteLine(closureFunc2());
+
+            Console.WriteLine(new string('-', 50));
+
+            ClosureClass closureClass = new(100);
+            Console.WriteLine(closureClass.ClosureFunc().Invoke());
+            Console.WriteLine(closureClass.ClosureFunc().Invoke());
+            Console.WriteLine(closureClass.ClosureFunc().Invoke());
+
         }
 
         static Func<int> ClosureFunc()
@@ -26,6 +34,29 @@
                 x++;
                 return x;
             };
+        }
+
+        
+    }
+
+    class ClosureClass
+    {
+        private int _x;
+
+        public ClosureClass(int X)
+        {
+            _x = X;
+        }
+
+        public Func<int> ClosureFunc()
+        {
+            return () => Increase(_x);
+        }
+
+        public static int Increase(int x)
+        {
+            x++;
+            return x;
         }
     }
 }
