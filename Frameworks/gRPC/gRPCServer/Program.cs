c2007f52fee046ddfeec033e917d1afd7acc1408
Yuriy Parashynets <yura.parashynets@gmail.com>
2026-05-05 19:17:17 +0300

    main - update gRPC Project

--- Frameworks/gRPC/gRPCServer/Program.cs
+++ Frameworks/gRPC/gRPCServer/Program.cs
@@ -16,6 +16,7 @@ public class Program
         // Configure the HTTP request pipeline.
         app.MapGrpcService<GreeterService>();
         app.MapGrpcService<PersonService>();
+        app.MapGrpcService<SynkService>();
         
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
---
+++ Frameworks/gRPC/gRPCServer/Services/SynkService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using static gRPCServer.Synk;
+
+namespace gRPCServer.Services;
+
+public class SynkService : SynkBase
+{
+    public override Task<TestResponseModel> GetTest(TestRequestModel request, ServerCallContext context)
+    {
+        return Task.FromResult(new TestResponseModel
+        {
+            Id = 10
+        });
+    }
+}
