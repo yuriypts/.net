595c5a70671310aed74e2f0ab2241b3670beae90
Yuriy Parashynets <yura.parashynets@gmail.com>
2026-05-13 16:20:05 +0300

    main - Add Angular GraphQL Project, install apollo-angular lib

---
+++ Frameworks/GraphQL/graphQLAngular/src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: '<%= endpoint %>',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
+  ],
+};
