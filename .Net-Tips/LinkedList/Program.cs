5c20b2f246c3f8fc955086d859eaae9f48e170b4
Yuriy Parashynets <yura.parashynets@gmail.com>
2026-07-10 15:34:06 +0300

    main - Add LinkedList

---
+++ .Net-Tips/LinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> linkedList = new();
+
+        linkedList.AddFirst(1);
+        linkedList.AddFirst(3);
+        linkedList.AddLast(2);
+
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine(new string('-', 20));
+
+        linkedList.Remove(1);
+
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine(new string('-', 20));
+
+        var node = linkedList.Find(3);
+        linkedList.AddAfter(node, 4);
+
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}
